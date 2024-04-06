7300c456ed18c80504838426c2a4ad4c2c8df2c7
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-06 08:50:43 +0000

    data types summary updated

--- 01_Basics/02_dataTypesSummary.js
+++ 01_Basics/02_dataTypesSummary.js
@@ -32,3 +32,35 @@ const myFunction = function(){
 console.log(typeof heros); // returns object
 console.log(typeof myObj); // returns object
 console.log(typeof myFunction); // returns function (function object)
+
+// ********************************** Memory Types ********************************************
+
+/*  Stack - This memory used in all primitive type data types
+    Heap - This memory used in all non primitive data types
+
+    In stack we are getting copy of a variable
+    In heap the changes made in original values
+ */
+
+// stack example
+
+let ytChannel = "ChaiAurCode"
+let anotherytChannel = ytChannel
+anotherytChannel = "TechnicalGuruji"
+
+console.log(ytChannel); // prints ChaiAurCode
+console.log(anotherytChannel); // TechnicalGuruji
+
+// heap example
+
+let userOne = {
+    email: "gm@yahoo.com",
+    upi: "user@axl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "gm@google.com"
+
+console.log(userOne.email); // prints gm@google.com
+console.log(userTwo.email); // prints gm@google.com
