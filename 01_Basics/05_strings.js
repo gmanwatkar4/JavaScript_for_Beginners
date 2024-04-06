34e4f61a67398f2d8ddc5d029869e3139da7bc04
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-06 09:30:58 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,36 @@
+// Strings
+// String Interpolation -> `` (backticks')
+
+let name = "Gaurav"
+let repoCount = 10
+
+console.log(`Hi ${name}, your repo count is ${repoCount}.`); // prints Hi Gaurav, your repo count is 10.
+
+// another way of string declaration
+
+let myName = new String("Sumit")
+console.log("My name: ", myName); // prints [String: 'Sumit']
+console.log("First character: ", myName[0]); // prints first character S
+console.log(myName.__proto__); // prints empty string
+console.log("Length:", myName.length); // prints Length: 5
+console.log("Upper case: ", myName.toUpperCase()); // prints SUMIT
+console.log("Char at index 2: ", myName.charAt([2])); // prints m
+console.log("Index of m: ", myName.indexOf('m'));
+
+const gameName = "CandyCrush"
+const newString = gameName.substring(0, 4)
+console.log(newString); // prints Cand
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString); // print nd
+
+const newStringOne = "  Gaurav  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://youtube.com/gaurav%20manwatkar"
+console.log(url.replace('%20', '-'))
+console.log(url.includes('gaurav'));
+
+const anotherGameName = "candy-crush-saga-soda"
+console.log(anotherGameName.split('-'));
