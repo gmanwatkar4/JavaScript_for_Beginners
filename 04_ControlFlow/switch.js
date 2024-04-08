a5655b4b3488e8a70ae55a5e6958544ecf5f053d
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-08 02:50:56 +0000

    switch statement

---
+++ 04_ControlFlow/switch.js
@@ -0,0 +1,43 @@
+// switch statement
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+const myMonth = "march"
+switch (myMonth) {
+    case "january":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
