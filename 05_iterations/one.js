0ce48d0a95b76451fe1236d62a2938e6ccc56689
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-09 02:04:34 +0000

    for break and continue

---
+++ 05_iterations/one.js
@@ -0,0 +1,43 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log("5 is the best number");
+    }
+    console.log(element);   
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`table of : ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        //console.log(`inner loop value ${j} and inner loop ${i}`);
+        console.log(i + ' * ' + j + ' = ' + i*j);
+    }
+}
+
+let myArray = ["flash", "batman", "superman"]
+    console.log("myArray length: ", myArray.length);
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log("myArray value:", element);
+}
+
+// break and continue
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log("5 detected!");
+        break
+    }
+    console.log(`value of i is ${i}`);
+}
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log("5 detected!");
+        continue // 1 times condition skipped
+    }
+    console.log(`value of i is ${i}`);
+}
