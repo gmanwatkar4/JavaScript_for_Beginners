070a32cfd4568cb3fdab1f78f02e05cdb376ebab
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-10 17:49:52 +0000

    more on objects

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,8 +1,15 @@
+// more on objects
+
+// creating tinderUser object
+
 const tinderUser = new Object()
 tinderUser.id = "123abc"
 tinderUser.name ="SAM"
 tinderUser.isLoggedIn = false
-console.log(tinderUser);
+
+console.log("tinderUser object : ", tinderUser);    // tinderUser object :  { id: '123abc', name: 'SAM', isLoggedIn: false }
+
+// userfullname object inside fullname object (objects in object)
 
 const regularUser = {
     email: "gm@gmail.com",
@@ -14,25 +21,28 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname);
-console.log(regularUser.fullname.userfullname);
-console.log(regularUser.fullname.userfullname.firstname);
+console.log(regularUser.fullname);  // { userfullname: { firstname: 'Gaurav', lastname: 'Manwatkar' } }
+console.log(regularUser.fullname.userfullname);     // { firstname: 'Gaurav', lastname: 'Manwatkar' }
+console.log(regularUser.fullname.userfullname.firstname);   // Gaurav
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
+// assiging objects to objects
+
+const obj1 = {1: "a", 2: "b"}   
+const obj2 = {3: "a", 4: "b"}   
 const obj3 = Object.assign({}, obj1, obj2)
 const obj4 = {...obj1, ...obj2}
-console.log(obj3);
-console.log(obj4);
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+console.log("obj3 : ", obj3);   // obj3 :  { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+console.log("obj4 : ", obj4);   // // obj4 :  { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
-console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+console.log("tinderUser : ", tinderUser);   // tinderUser :  { id: '123abc', name: 'SAM', isLoggedIn: false }
+console.log("keys : ", Object.keys(tinderUser));    // keys :  [ 'id', 'name', 'isLoggedIn' ]
+console.log("values : ", Object.values(tinderUser));    // values :  [ '123abc', 'SAM', false ]
+console.log("entries : ", Object.entries(tinderUser));  // entries :  [ [ 'id', '123abc' ], [ 'name', 'SAM' ], [ 'isLoggedIn', false ] ]
 
-//******************************* Destructuring ************************************
+console.log("hasOwnProperty : ", tinderUser.hasOwnProperty("isLoggedIn"));  // hasOwnProperty :  true
+
+//**************************************** Destructuring ************************************
 
 const course = {
     coursename: "js in hindi",
@@ -40,7 +50,7 @@ const course = {
     courseInstructor: "Gaurav"
 }
 
-// course.courseInstructor
+console.log("course instructor : ", course.courseInstructor) // course instructor :  Gaurav
 
 const {courseInstructor : instructor} = course // destructuring of object
-console.log("Course Instructor:", instructor);
+console.log("Course Instructor : ", instructor); // Course Instructor : Gaurav
