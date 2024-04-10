b994b052624bd559dfa993bae67fc65e6128a000
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-10 17:23:21 +0000

    array and array methods

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -6,46 +6,68 @@
 *A deep copy of an object is a copy whose properties do not share the same reference point (stack)
 */
 
-const myArr = [1, 2, 3, 4, 5] // 1,2,3,4 are called array elements
-console.log("0th index: ", myArr[0])
+// number array
+
+const myArr = [1, 2, 3, 4, 5]       // 1,2,3,4,5 are called array elements
+console.log("0th index: ", myArr[0])    // 0th index:  1
+
+// string array
 
 const myheros = ["Shaktiman", "Naagraj"]
 
 const myArr1 = new Array(5, 4, 3, 2, 1)
-console.log("0th index: ", myArr1[0])
+console.log("0th index: ", myArr1[0])   // 0th index:  5
 
 // Array Methods
+
+// push() method
 
 myArr.push(6) // add 6 at the end of array
 myArr.push(7) // adds 7 at the end of array
 myArr.pop() // removes last element of an array
-console.log(myArr);
+console.log("myArr : ", myArr);     // myArr:  [ 1, 2, 3, 4, 5, 6 ]
+
+// unshift() method 
 
 myArr.unshift(0)
 console.log(myArr); // adds 0 at the beginning of the array
 
+// shift() method
+
 myArr.shift()
 console.log(myArr); // removes first index of an array
 
-console.log("Is value exist: ", myArr.includes(9)); // false
-console.log("Index of 5: ", myArr.indexOf(5));
+// includes() and indexOf()
+
+console.log("Is value exist: ", myArr.includes(9));     // Is value exist:  false
+console.log("Index of 5: ", myArr.indexOf(5));      // Index of 5:  4
+
+// join() method
 
 const newArr = myArr.join()
-console.log("myArr: ", myArr);
-console.log("newArr: ", newArr);
-console.log("type of myArr: ", typeof myArr);
-console.log("type of newArr: ", typeof newArr);
+console.log("myArr: ", myArr);  // myArr:  [ 1, 2, 3, 4, 5, 6 ]
+console.log("newArr: ", newArr);    // newArr:  1,2,3,4,5,6
+console.log("type of myArr: ", typeof myArr);   // type of myArr:  object
+console.log("type of newArr: ", typeof newArr); // type of newArr:  string
 
-// slice, splice
+// slice(), splice() methods
 
-console.log("A: ", myArr);
+console.log("A: ", myArr);  // A:  [ 1, 2, 3, 4, 5, 6 ]
 
 const myn1 = myArr.slice(1, 3)
-console.log("slice result: ", myn1); // only prints 1st and 2nd index elements, ignores from end inputed element
 
-console.log("B: ", myArr);
+// only prints 1st and 2nd index elements, ignores from end inputed element
+
+console.log("slice result: ", myn1); // slice result:  [ 2, 3 ]
+
+console.log("B: ", myArr);  // B:  [ 1, 2, 3, 4, 5, 6 ]
 
 const myn2 = myArr.splice(1, 3)
-console.log("splice result: ", myn2); // moves the values from original array, moves 1st, 2nd and 3rd elements
 
-console.log("C: ", myArr); // splice manipulates the array
+// moves the values from original array, moves 1st, 2nd and 3rd elements
+
+console.log("splice result: ", myn2);   // splice result:  [ 2, 3, 4 ]
+
+// splice manipulates the array
+
+console.log("C: ", myArr);  // C:  [ 1, 5, 6 ]
