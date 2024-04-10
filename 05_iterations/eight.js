26a1d9d49ed046ece8a9dba56cfe59c7f0ebe9b5
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-10 04:28:44 +0000

    reduce function

---
+++ 05_iterations/eight.js
@@ -0,0 +1,35 @@
+// reduce function
+
+const myNums = [1, 2, 3, 4, 5]
+
+const myTotal = myNums.reduce( function (acc, curval) {
+    console.log(`acc: ${acc} and curval: ${curval}`);
+    return acc + curval
+}, 0)
+
+console.log(myTotal);
+
+const myTotal1 = myNums.reduce( (acc, curval) => acc+curval, 0)
+console.log(myTotal1);
+
+const shoppingCart = [
+    {
+        itemname: "js course",
+        price: 999
+
+    },
+    {
+        itemname: "mobile dev course",
+        price: 1999
+
+    },
+    {
+        itemname: "js course",
+        price: 9999
+
+    }
+
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+console.log(priceToPay);
