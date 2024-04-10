1f56e0df7b62ad4d1e7e0c4228a8e8393417c3d2
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-10 16:40:25 +0000

    strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -9,13 +9,13 @@ console.log(`Hi ${name}, your repo count is ${repoCount}.`); // prints Hi Gaurav
 // another way of string declaration
 
 let myName = new String("Sumit")
-console.log("My name: ", myName); // prints [String: 'Sumit']
-console.log("First character: ", myName[0]); // prints first character S
-console.log(myName.__proto__); // prints empty string
-console.log("Length:", myName.length); // prints Length: 5
-console.log("Upper case: ", myName.toUpperCase()); // prints SUMIT
+console.log("My name: ", myName);                   // prints [String: 'Sumit']
+console.log("First character: ", myName[0]);        // prints first character S
+console.log(myName.__proto__);                      // prints empty string
+console.log("Length:", myName.length);              // prints Length: 5
+console.log("Upper case: ", myName.toUpperCase());  // prints SUMIT
 console.log("Char at index 2: ", myName.charAt([2])); // prints m
-console.log("Index of m: ", myName.indexOf('m'));
+console.log("Index of m: ", myName.indexOf('m'));       // prints index 2
 
 const gameName = "CandyCrush"
 const newString = gameName.substring(0, 4)
@@ -25,12 +25,12 @@ const anotherString = gameName.slice(-8, 4)
 console.log(anotherString); // print nd
 
 const newStringOne = "  Gaurav  "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne);          // prints " Gaurav  "
+console.log(newStringOne.trim());   // prints "Gaurav"
 
 const url = "https://youtube.com/gaurav%20manwatkar"
-console.log(url.replace('%20', '-'))
-console.log(url.includes('gaurav'));
+console.log(url.replace('%20', '-'))        // prints https://youtube.com/gaurav-manwatkar               
+console.log(url.includes('gaurav'));        // returns true
 
 const anotherGameName = "candy-crush-saga-soda"
-console.log(anotherGameName.split('-'));
+console.log(anotherGameName.split('-'));        // prints [ 'candy', 'crush', 'saga', 'soda' ]
