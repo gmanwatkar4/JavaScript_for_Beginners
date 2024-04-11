42ff771b372319d188a1fc0c959c2a99de4a5f1b
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-11 01:54:07 +0000

    functions in javascript

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,3 +1,7 @@
+// functions
+
+// function example
+
 function sayMyName(){
     console.log("G");
     console.log("A");
@@ -9,31 +13,40 @@ function sayMyName(){
 
 sayMyName()
 
-function addTwoNumbers(number1, number2){           // number1, number2 are called parameters 
-    console.log("Addition:", number1 + number2);
+// add two numbers using functions
+
+// number1, number2 are called parameters 
+function addTwoNumbers(number1, number2){           
+    console.log("Addition:", number1 + number2);    // Addition: 10
 }
 
 addTwoNumbers(5, 5) // 5, 5 are called arguments
+
+// another way of writing function
 
 function addTwoNumbers1(number1, number2){ 
    const result = number1 + number2
    return result
 }
-
 const result = addTwoNumbers1(6, 6)
-console.log("Result: ", result);
+console.log("Result: ", result);        // Result:  12
+
+// passing string as parameter
 
 function loginUserMessage(username){
     return `${username}, just logged in....`
 }
-console.log(loginUserMessage("gaurav"));
-console.log(loginUserMessage()); // when value is not passed to function it returns undefined
+console.log(loginUserMessage("gaurav"));    // gaurav, just logged in....
+
+// when value is not passed to function it returns undefined
+console.log(loginUserMessage());    // undefined, just logged in....
+
+// ... rest operator
 
 function calculateCartPrice(...num1){       // ... rest operator
     return num1
 }
-
-console.log(calculateCartPrice(200, 300, 500));
+console.log("cart values : ", calculateCartPrice(200, 300, 500));   // cart values :  [ 200, 300, 500 ]
 
 // passing object to the function
 
@@ -43,7 +56,7 @@ const user = {
 }
 
 function handleObject(anyObject){
-    console.log(`username is ${anyObject.username} and the price is ${anyObject.price}`);
+    console.log(`username is ${anyObject.username} and the price is ${anyObject.price}`);   // username is gaurav and the price is 199
 }
 
 handleObject(user) // passing object "user" to function "handleObject"
@@ -53,7 +66,7 @@ handleObject(user) // passing object "user" to function "handleObject"
 handleObject({
     username: "Faizal",
     price: 399
-})
+})      // username is Faizal and the price is 399
 
 // passing array to the function
 
@@ -62,8 +75,8 @@ const myNewArr = [100, 200, 300, 400, 500]
 function returnValue(getArr){
     return getArr[2]
 }
-console.log("return array element: ", returnValue(myNewArr));
+console.log("return array element: ", returnValue(myNewArr));   // return array element:  300
 
 // another way of passing array to the function
 
-console.log("new element: ", returnValue([100, 200, 300, 400, 500]));
+console.log("new element: ", returnValue([100, 200, 300, 400, 500]));   // new element:  300
