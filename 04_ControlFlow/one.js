d64b6d836dede6fdaa2cc568bd445a373ba387ba
gmanwatkar4 <39916428+gmanwatkar4@users.noreply.github.com>
2024-04-11 02:12:51 +0000

    if statement in javascript

--- 04_ControlFlow/one.js
+++ 04_ControlFlow/one.js
@@ -1,4 +1,4 @@
-// if
+// if statement in javascript
 
 /* 
 Comparision operators :
@@ -11,7 +11,7 @@ const temperature = 50
 if(temperature === 55){
     console.log("less than 50");
 } else{
-    console.log("temperature is greater than 55");
+    console.log("temperature is greater than 55");  // temperature is greater than 55
 }
 
 const score = 200
@@ -19,15 +19,15 @@ const power = "fight"
 
 if(score > 100){
     const power = "fly"
-    console.log(`user power: ${power}`);
+    console.log(`user power: ${power}`);    // user power: fly
 }
 
-console.log(`user power: ${power}`);
+console.log(`user power: ${power}`);    // user power: fight
 
 // short hand notation
 
 const balance = 1000
-if (balance > 500) console.log("true");
+if (balance > 500) console.log("true");     // true
 
 // nesting
 
@@ -38,10 +38,10 @@ if (balance < 500) {
 } else if (balance < 900) {
     console.log("less than 900");
 }else{
-    console.log("less than 1200");
+    console.log("less than 1200");  // less than 1200
 }
 
-// 
+// condition check via logical operators
 
 const userLoggedIn = true
 const debitCard = true
@@ -49,10 +49,9 @@ const loggedInFromGoogle = false
 const loggedInFromEmail = true
 
 if (userLoggedIn && debitCard) {
-    console.log("allow to buy courses");
+    console.log("allow to buy courses");    // allow to buy courses
 }
 
 if (loggedInFromGoogle || loggedInFromEmail) {
-    console.log("user logged in");
+    console.log("user logged in");  // user logged in
 }
-
